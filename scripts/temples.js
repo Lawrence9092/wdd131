6d837f85d7ce8e82d01481ab60c929fdb8897ec6
Lawrence9092 <lngwenya@byupathway.edu>
2026-01-20 11:30:31 +0200

    Cleaning

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,15 @@
-const menuButton = document.querySelector("#menuButton");
-const navMenu = document.querySelector("#navMenu");
+// Footer dynamic dates
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+// Hamburger menu toggle
+const menuButton = document.getElementById("menuButton");
+const navMenu = document.getElementById("navMenu");
 
 menuButton.addEventListener("click", () => {
   navMenu.classList.toggle("open");
 
+  // Toggle icon
   if (navMenu.classList.contains("open")) {
     menuButton.textContent = "✖";
   } else {
