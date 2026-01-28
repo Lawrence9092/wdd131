2c262430ce4ef00e49269977b6b519742586b096
Lawrence9092 <lngwenya@byupathway.edu>
2026-01-28 19:56:03 +0200

    html, css, js structure

---
+++ scripts/place.js
@@ -0,0 +1,26 @@
+// Footer dates
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+// Static weather values
+const temperature = 24; // °C
+const windSpeed = 10;   // km/h
+
+// Wind chill calculation function (metric)
+function calculateWindChill(temp, speed) {
+  return (
+    13.12 +
+    0.6215 * temp -
+    11.37 * Math.pow(speed, 0.16) +
+    0.3965 * temp * Math.pow(speed, 0.16)
+  ).toFixed(1);
+}
+
+// Wind chill conditions
+let windChill = "N/A";
+
+if (temperature <= 10 && windSpeed > 4.8) {
+  windChill = `${calculateWindChill(temperature, windSpeed)} °C`;
+}
+
+document.getElementById("windchill").textContent = windChill;
